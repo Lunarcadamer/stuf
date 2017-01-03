d422a3116171db66d4a695e6d09dd70d7832bc97
Lunarcadamer <wasp.7000@gmail.com>
2017-01-03 18:11:40 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -8,6 +8,8 @@
 typedef struct shadowNode {
     char username[32];
     char hash[128];
+    char recoveredPassword[64];
+    char timeData[26];
     struct shadowNode * next;
 } S_NODE;	// Defining node structure
 
@@ -64,14 +66,18 @@ void release_words(S_NODE * curPtr) {
     
 }	// To clear the linked list after usage
 
-void getTime() {
-	time_t rawtime;
- 	struct tm * timeinfo;
+char * getTime() {
+	time_t timer;
+    struct tm* tm_info;
+    static char timeStr[32];
 
-  	time ( &rawtime );
-  	timeinfo = localtime ( &rawtime );
+    time(&timer);
+    tm_info = localtime(&timer);
 
-  	printf("%s", asctime(timeinfo));
+    strftime(timeStr, 26, "%Y:%m:%d %H:%M:%S", tm_info);
+
+    return timeStr;
+
 }	// Function to print time
 
 int inc(char *c){
@@ -101,9 +107,8 @@ int main() {
 	int i, j; //Loop Integers
 	int len; //Length of guess
 	char *c = malloc((n+1)*sizeof(char)); //Brute force guess
-	
-	printf("Program Started: ");
-	getTime();
+
+	printf("Program Started: %s \n", getTime());
 
 	FILE * fp;
     fp = fopen("shadow.txt", "r");
@@ -118,6 +123,8 @@ int main() {
     }
 
     fclose(fp);
+
+    S_NODE * headPtr = rootPtr; // Backup of original pointer
 	
 	for(i=1;i<=n;i++){
     	for(j=0;j<i;j++) c[j]='0';
@@ -129,8 +136,9 @@ int main() {
 		   		result = crypt(c, rootPtr->hash);
 		   		
 				if(strcmp(result, rootPtr->hash) == 0) {	//if result = secret, strcmp returns 0
-					printf("User: %s Password: %6s recovered at ", rootPtr->username,c);
-					getTime();
+					//printf("User: %s Password: %6s recovered at [%s]\n", rootPtr->username, c, getTime());
+					strcpy(rootPtr->recoveredPassword, c);
+					strcpy(rootPtr->timeData, getTime());
 					rootPtr = rootPtr->next;
 					i=1;
 					j=0;
@@ -143,10 +151,15 @@ int main() {
 
         } while(inc(c));
     }
-    
-    release_words(rootPtr);
 
-    printf("Program Ended: ");
-    getTime();
+    printf("Program Ended: %s \n\n", getTime());
+
+    while (headPtr != NULL) {
+    	printf("User: %s Password: %6s recovered at [%s]\n", headPtr->username, headPtr->recoveredPassword, headPtr->timeData);
+    	headPtr = headPtr->next;
+    }
+
+    release_words(headPtr);
+    release_words(rootPtr);
 
 }	// Main
